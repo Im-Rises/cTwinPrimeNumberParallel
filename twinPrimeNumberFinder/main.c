0bf942043b84572f6d1708c704370293779bdd64
Im-Rises <quentin-morel88@hotmail.com>
2022-11-24 04:59:46 -0500

    Added new ver

--- twinPrimeNumberFinder/main.c
+++ twinPrimeNumberFinder/main.c
@@ -14,10 +14,12 @@ void printPrimeNumbers(int* array, int n, int* outputArray);
 
 void siegeOfEratosthenesV1();
 void siegeOfEratosthenesV2();
+void siegeOfEratosthenesV3();
 
 int main(int argc, char* argv[]) {
     siegeOfEratosthenesV1();
     siegeOfEratosthenesV2();
+    siegeOfEratosthenesV3();
     return 0;
 }
 
@@ -79,7 +81,7 @@ void printPrimeNumbers(int* array, int n, int* outputArray) {
             count++;
         }
     }
-    printf("There are %d prime numbers between 0 and %d", count, n);
+    printf("There are %d prime numbers between 0 and %d\n", count, n);
 }
 
 void siegeOfEratosthenesV1() {
@@ -91,6 +93,7 @@ void siegeOfEratosthenesV1() {
     int k = 2;
     while (k * k <= n)
     {
+        // a) Mark all multiples of k between k^2 and n as non-prime
         for (int j = k * k; j < n; j += k)
         {
             if (j % k == 0)
@@ -98,6 +101,7 @@ void siegeOfEratosthenesV1() {
                 outputArray[j] = 0;
             }
         }
+        // b) Find the smallest number greater than k that is not marked
         for (int i = k + 1; i < n; i++)
         {
             if (outputArray[i] == 1)
@@ -115,31 +119,45 @@ void siegeOfEratosthenesV1() {
 }
 
 void siegeOfEratosthenesV2() {
-    //    int n = 100;
-    //    int* array = allocateMemory(sizeof(int) * n);
-    //    int* outputArray = allocateMemory(sizeof(int) * n);
-    //    populateArray(array, n, outputArray);
-    //
-    //    int k = 2;
-    //    while (k * k <= n)
-    //    {
-    //        for (int i = k * k; i <= n; i += k)
-    //        {
-    //            outputArray[i] = 0;
-    //        }
-    //        for (int i = k + 1; i <= n; i++)
-    //        {
-    //            if (outputArray[i] == 1)
-    //            {
-    //                k = i;
-    //                break;
-    //            }
-    //        }
-    //        //            k++;
-    //    }
-    //
-    //    printPrimeNumbers(array, n, outputArray);
-    //
-    //    free(array);
-    //    free(outputArray);
+    int n = 100;
+    int* array = allocateMemory(sizeof(int) * n);
+    int* outputArray = allocateMemory(sizeof(int) * n);
+    populateArray(array, n, outputArray);
+
+    int k = 2;
+    while (k * k <= n)
+    {
+        for (int i = k * k; i < n; i += k)
+        {
+            outputArray[i] = 0;
+        }
+        k++;
+    }
+
+    printPrimeNumbers(array, n, outputArray);
+
+    free(array);
+    free(outputArray);
+}
+
+void siegeOfEratosthenesV3() {
+    int n = 100;
+    int* array = allocateMemory((n + 1) * sizeof(int));
+    int* prime = allocateMemory((n + 1) * sizeof(int));
+
+    populateArray(array, n, prime);
+
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (prime[p] == 1)
+        {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = 0;
+        }
+    }
+
+    printPrimeNumbers(array, n, prime);
+
+    free(prime);
+    free(array);
 }
